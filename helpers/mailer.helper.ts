5cb35c65e912e653e5bb405cbf894c05a4e2dbe9
DELL <thanhnamnb1004@gmail.com>
2026-05-20 17:05:00 +0700

    mpussh mail

--- helpers/mailer.helper.ts
+++ helpers/mailer.helper.ts
@@ -1,41 +1,20 @@
 import nodemailer from 'nodemailer';
-import { google } from 'googleapis';
-import { Agent } from 'https';
 import dns from 'dns';
 
-// Ép dùng IPv4
 dns.setDefaultResultOrder('ipv4first');
 
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
-const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
-const REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN!;
 const SENDER_EMAIL = process.env.SENDER_EMAIL!;
-
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  'https://developers.google.com/oauthplayground'
-);
-
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+const GMAIL_APP_PASSWORD = process.env.GMAIL_APP_PASSWORD!;
 
 export const sendOtpEmail = async (toEmail: string, otp: string) => {
-  const accessToken = await oAuth2Client.getAccessToken();
-
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
-      type: 'OAuth2',
       user: SENDER_EMAIL,
-      clientId: CLIENT_ID,
-      clientSecret: CLIENT_SECRET,
-      refreshToken: REFRESH_TOKEN,
-      accessToken: accessToken.token!,
+      pass: GMAIL_APP_PASSWORD,
     },
-    // Ép IPv4
-    tls: {
-      rejectUnauthorized: false
-    }
   });
 
   await transporter.sendMail({
