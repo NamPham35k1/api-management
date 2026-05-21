19dd5dfed3515444ca781ebe032522be2c7ee351
DELL <thanhnamnb1004@gmail.com>
2026-05-21 11:22:19 +0700

    fix gmail

--- helpers/mailer.helper.ts
+++ helpers/mailer.helper.ts
@@ -1,27 +1,10 @@
-import nodemailer from 'nodemailer';
-import dns from 'dns';
+import { Resend } from 'resend';
 
-dns.setDefaultResultOrder('ipv4first');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendOtpEmail = async (toEmail: string, otp: string) => {
-  const SENDER_EMAIL = process.env.SENDER_EMAIL!;
-  const GMAIL_APP_PASSWORD = process.env.GMAIL_APP_PASSWORD!;
-
-  console.log('EMAIL:', SENDER_EMAIL);
-  console.log('PASS:', GMAIL_APP_PASSWORD);
-
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: SENDER_EMAIL,
-      pass: GMAIL_APP_PASSWORD,
-    },
-  });
-
-  await transporter.sendMail({
-    from: `"IPTV App" <${SENDER_EMAIL}>`,
+  await resend.emails.send({
+    from: 'onboarding@resend.dev',
     to: toEmail,
     subject: 'Mã xác thực OTP',
     html: `<h2>Mã OTP của bạn: <b>${otp}</b></h2><p>Mã có hiệu lực trong 5 phút.</p>`,
